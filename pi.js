79403f3c73adada8cea875d9304352d98bf6b1a4
Thomas Schmidt <thomas.michel.schmidt@gmail.com>
2026-01-21 16:19:06 +0100

    dev du prog

---
+++ pi.js
@@ -0,0 +1,38 @@
+import decimalData from "./decimals.json" with {"type": "json"};
+import colorData from "./colors.json" with {"type": "json"};
+
+window.addEventListener("DOMContentLoaded", () => {
+    const canvaWidth = 500;
+    const canvaHeight = 500;
+    const pixSize = 5;
+
+    const decimals = decimalData.values
+        .replace(/\s+/g, "")
+        .split("")
+        .map(Number);
+    console.log(decimals.length)
+    const colors = colorData.values
+
+    // creer une div pixel + un id personnalisé + background color
+    const divRepresentation = document.getElementById("representation");
+    const canva = document.createElement("canvas")
+    canva.width = canvaWidth
+    canva.height = canvaHeight
+    divRepresentation.appendChild(canva)
+
+    const pix = canva.getContext("2d");
+
+    let xOrigin = 0;
+    let yOrigin = 0;
+
+    for (let i = 0; i < decimals.length; i++) {
+        const colorPix = decimals[i]
+        pix.fillStyle = colors[colorPix];
+        pix.fillRect(xOrigin, yOrigin, pixSize, pixSize);
+        xOrigin = xOrigin + pixSize;
+        if (xOrigin === canvaWidth) {
+            xOrigin = 0
+            yOrigin = yOrigin + pixSize
+        }
+    }
+})
